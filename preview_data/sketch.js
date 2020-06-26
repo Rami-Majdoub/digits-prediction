f4fb924652a457e9e96a19ffd8d09f5da01e259a
Rami majdoub <rami7856@gmail.com>
2020-06-26 22:48:56 +0100

    added preview_data

---
+++ preview_data/sketch.js
@@ -0,0 +1,99 @@
+const samplePerColumn = 10;
+const samplePerRow = 10;
+
+const edges = 8;
+const smallEdgeSize = 5;
+const pading = 2;
+const edgeSize = smallEdgeSize * edges;
+
+async function setup() {
+  await loadFile('./data/optdigits.tra.txt');
+  
+  const w = (edgeSize + pading) * samplePerColumn;
+  const h = (edgeSize + pading) * samplePerRow;
+  // I will draw the representation of the input and its value in the right
+  const canvas = createCanvas(w, h);
+  
+  background(0);
+  noLoop();
+  noStroke();
+  
+  drawSamples();
+  
+  // console.log('drawing Complete');
+}
+
+function drawSamples(){
+  for (let row = 0; row < samplePerRow; row++){
+    for (let col = 0; col < samplePerColumn; col++){
+      
+      const index = col + row * samplePerColumn;
+      const x = (edgeSize + pading) * col;
+      const y = (edgeSize + pading) * row;
+      
+      drawSample(inputs[index], x, y);
+    }
+  }
+}
+
+function drawSample(sample, x, y){
+  for(let row = 0; row < edges; row++){
+    for(let col = 0; col < edges; col++){
+      drawSampleValue(sample, x, y, row, col);
+    }
+  }
+}
+function drawSampleValue(values, sampleX, sampleY, row, col){
+  
+  const x = sampleX + col * smallEdgeSize;
+  const y = sampleY + row * smallEdgeSize;
+  
+  var index = values[col + row * edges];
+  
+  const grayValue = map(index, 0, 16, 0, 255);
+  
+  fill(grayValue);
+  
+  rect(x, y, smallEdgeSize, smallEdgeSize);
+}
+
+var labels = [];
+var inputs = [];
+async function loadFile(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    
+    
+    var samples = data.split('\n');
+    for(s of samples){
+      const lastComma = s.lastIndexOf(',');
+      
+      var label = s.substring(lastComma + 1, s.length);
+      labels.push(parseInt(label));
+      
+      var inputs_ = s.substring(0, lastComma).split(',');
+      var features = [];
+      for (f of inputs_){
+        features.push(parseInt(f));
+      }
+      inputs.push(features);
+    }
+    
+    // remove the last one
+    labels.pop();
+    inputs.pop();
+    
+    console.log('loading Complete')
+    
+    // console.log(inputs.length);
+    // console.log(labels.length);
+    // console.log(inputs);
+    // console.log(labels);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+
