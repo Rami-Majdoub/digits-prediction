6a72fc81e0e42702eeac3129b979ecc3043e761a
Rami majdoub <rami7856@gmail.com>
2020-06-27 19:16:41 +0100

    added bootstrap

--- create_model/sketch.js
+++ create_model/sketch.js
@@ -3,33 +3,55 @@ const knnClassifier = ml5.KNNClassifier();
 function setup() {
   // the file location
   const fileUrl = '../data/optdigits.tra.txt'
+
+  createAlertDanger('Creating a new model << this process will take few seconds or minutes >>');
+
   loadFileAndPrintToConsole(fileUrl);
+}
+
+function createAlertDanger(text) {
+  let div = createDiv(text);
+  div.addClass('alert');
+  div.addClass('alert-danger');
+}
+
+function createAlertInfo(text) {
+  let div = createDiv(text);
+  div.addClass('alert');
+  div.addClass('alert-info');
+}
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function loadFileAndPrintToConsole(url) {
   try {
+    await createAlertInfo('loading file content');
+
     const response = await fetch(url);
     const data = await response.text();
-    
+
+    await sleep(500);
+    createAlertInfo('loading data into variables');
     // process data
     var labels = [];
     var inputs = [];
-    
+
     var samples = data.split('\n');
-    for(s of samples){
+    for (s of samples) {
       const lastComma = s.lastIndexOf(',');
-      
+
       var label = s.substring(lastComma + 1, s.length);
       labels.push(parseInt(label));
-      
+
       var inputs_ = s.substring(0, lastComma).split(',');
       var features = [];
-      for (f of inputs_){
+      for (f of inputs_) {
         features.push(parseInt(f));
       }
       inputs.push(features);
     }
-    
+
     // remove the last one
     labels.pop();
     inputs.pop();
@@ -37,16 +59,20 @@ async function loadFileAndPrintToConsole(url) {
     // console.log(labels.length);
     // console.log(inputs);
     // console.log(labels);
+
+    await sleep(500);
+    createAlertInfo('trainnig classifier');
     var i;
-    for(i =0; i<inputs.length; i++){
+    for (i = 0; i < inputs.length; i++) {
       knnClassifier.addExample(inputs[i], labels[i]);
     }
     // knnClassifier.train(whileTraining);
+
+    await sleep(500);
+    createAlertInfo('downloading file');
     knnClassifier.save();
-    
-    console.log('done');
-    
+
   } catch (err) {
-    console.error(err);
+    createAlertDanger(err);
   }
 }
--- sketch.js
+++ sketch.js
@@ -12,60 +12,64 @@ const knnClassifier = ml5.KNNClassifier();
 
 function setup() {
   initElements();
-  
+
   predictionText.html('i\'m concentrating');
   knnClassifier.load('./models/myKNN_2.json', modelLoaded);
-  
+
   fill(penColor);
   stroke(penColor); // no edges
   strokeWeight(lineW);
   clearBackground();
 }
 
-function modelLoaded(){
+function modelLoaded() {
   predictionText.html('ok i\'m ready to play');
   console.log('model ready');
 }
 
-function clearBackground(){background(bgColor);}
+function clearBackground() { background(bgColor); }
 
-function initElements(){
+function initElements() {
   const canvas = createCanvas(edge, edge);
   canvas.parent('canvasContainer');
-  
+
   resetBtnA = select('#clearButton');
   resetBtnA.mousePressed(clearBackground);
-  
-  createElement('h2', 'Let\'s play a game, you draw a digit from 0 to 9 and i will try to guess it, Wanna play?');
-  predictionText = createElement('h3', '');
-  
+
+  predictionText = select('#prediction');
+
 }
 
 // to draw a continuous line
 var prevX, prevY;
 
-function mouseDragged(){
-  if(prevX !== undefined && prevY !== undefined){
+function mouseDragged() {
+  if (prevX !== undefined && prevY !== undefined) {
     line(prevX, prevY, mouseX, mouseY);
   }
   prevX = mouseX;
   prevY = mouseY;
 }
 
-function mouseReleased() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function mouseReleased() {
   prevX = undefined;
   prevY = undefined;
-  
-  predict();
+
   predictionText.html('i\'m still guessing !');
+  await sleep(100);
+  predict();
   console.log('mouse released!');
 }
 
-function predict(){
-  if (knnClassifier !== undefined){ // still loading
+function predict() {
+  if (knnClassifier !== undefined) { // still loading
     // transform the canvas to input
     let input = transformCanvasToFeatures();
-  
+
     // use the model to predict the digit
     knnClassifier.classify(input, gotResults);
   }
@@ -78,27 +82,27 @@ function gotResults(err, result) {
   if (result.confidencesByLabel) {
     const confidences = result.confidencesByLabel;
     console.log(confidences);
-    
-    if (result.label){
+
+    if (result.label) {
       predictionText.html('i think that this is a ' + result.label);
     }
   }
 }
 
-function transformCanvasToFeatures(){
+function transformCanvasToFeatures() {
   loadPixels();
   let d = pixelDensity();
-  
+
   let f = [];
-  for (let x = 0; x < nbSquare; x++){
-    for (let y = 0; y < nbSquare; y++){
+  for (let x = 0; x < nbSquare; x++) {
+    for (let y = 0; y < nbSquare; y++) {
       const left = x * nbPixelsInSquare;
       const top = y * nbPixelsInSquare;
-      
+
       const grayValue = getRectAvg(left, top, nbPixelsInSquare, d);
       // ~~ converts a float to an integer
       // for more visit https://stackoverflow.com/questions/34077449/fastest-way-to-cast-a-float-to-an-int-in-javascript/34077505
-      
+
       f.push(~~map(grayValue, 0, 255, 0, 16));
     }
   }
@@ -107,7 +111,7 @@ function transformCanvasToFeatures(){
   return f;
 }
 
-function getRectAvg(left, top, pixels, density){
+function getRectAvg(left, top, pixels, density) {
   var s = 0;
   for (let i = 0; i < pixels; i++) {
     for (let j = 0; j < pixels; j++) {
@@ -117,13 +121,13 @@ function getRectAvg(left, top, pixels, density){
   return s / (pixels * pixels);
 }
 
-function logCanvasPixels(){
+function logCanvasPixels() {
   loadPixels();
   let d = pixelDensity();
 
   let p = [];
   for (let i = 0; i < width; i++) {
-    for (let j = 0; j < height; j++){
+    for (let j = 0; j < height; j++) {
       p.push(getPixelGray(i, j, d));
     }
   }
@@ -131,14 +135,14 @@ function logCanvasPixels(){
   console.log(p.length);
 }
 
-function getPixelGray(x, y, density){
+function getPixelGray(x, y, density) {
   var s = 0;
   const d = density;
   for (let i = 0; i < d; i++) {
     for (let j = 0; j < d; j++) {
       // loop over
       index = 4 * ((y * d + j) * width * d + (x * d + i));
-      
+
       s += pixels[index];
       // pixels[index] = r;
       // pixels[index+1] = g;
@@ -149,4 +153,4 @@ function getPixelGray(x, y, density){
   return s / (d * d);
 }
 
-function draw() {}
+function draw() { }
