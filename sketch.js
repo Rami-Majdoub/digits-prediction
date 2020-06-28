9534b7d9075ee15704da2352ebacc62182759460
Rami majdoub <rami7856@gmail.com>
2020-06-28 03:17:16 +0100

    improved the model performance and added the 'see what the model sees'

--- sketch.js
+++ sketch.js
@@ -4,22 +4,30 @@ const penColor = 255; // white
 
 // the number of squares the image is divided to (look at the training data)
 const nbSquare = 8;
-const nbPixelsInSquare = 10;
-const edge = nbSquare * nbPixelsInSquare;
+const maxNbPixelsInSquare = 10;
+const edge = nbSquare * maxNbPixelsInSquare;
+
+const minTouchPos = new p5.Vector(edge, edge);
+const maxTouchPos = new p5.Vector(0, 0);
 
 // Create a KNN classifier
 const knnClassifier = ml5.KNNClassifier();
 
 function setup() {
   initElements();
+  img = createGraphics(edge, edge);
 
   predictionText.html('i\'m concentrating');
   knnClassifier.load('./models/myKNN_2.json', modelLoaded);
 
+  setDrawingProperties();
+  clearCanvas();
+}
+
+function setDrawingProperties() {
   fill(penColor);
   stroke(penColor); // no edges
   strokeWeight(lineW);
-  clearBackground();
 }
 
 function modelLoaded() {
@@ -27,28 +35,50 @@ function modelLoaded() {
   console.log('model ready');
 }
 
-function clearBackground() { background(bgColor); }
+function clearCanvas() {
+  background(bgColor);
+
+  minTouchPos.set(edge, edge);
+  maxTouchPos.set(0, 0);
+}
 
 function initElements() {
   const canvas = createCanvas(edge, edge);
   canvas.parent('canvasContainer');
 
   resetBtnA = select('#clearButton');
-  resetBtnA.mousePressed(clearBackground);
+  resetBtnA.mousePressed(clearCanvas);
 
   predictionText = select('#prediction');
+}
 
+function mouseInCanvas() {
+  return 0 <= mouseX && mouseX <= width && 0 <= mouseY && mouseY <= height;
 }
 
 // to draw a continuous line
 var prevX, prevY;
 
 function mouseDragged() {
-  if (prevX !== undefined && prevY !== undefined) {
-    line(prevX, prevY, mouseX, mouseY);
+  if (mouseInCanvas()) {
+    if (prevX !== undefined && prevY !== undefined) {
+      line(prevX, prevY, mouseX, mouseY);
+    }
+
+    const currentMinX = (mouseX - lineW);
+    const currentMaxX = (mouseX + lineW);
+    const currentMinY = (mouseY - lineW);
+    const currentMaxY = (mouseY + lineW);
+
+    if (minTouchPos.x > currentMinX && currentMinX >= 0) minTouchPos.x = currentMinX;
+    if (minTouchPos.y > currentMinY && currentMinY >= 0) minTouchPos.y = currentMinY;
+    if (maxTouchPos.x < currentMaxX && currentMaxX <= width) maxTouchPos.x = currentMaxX;
+    if (maxTouchPos.y < currentMaxY && currentMaxY <= height) maxTouchPos.y = currentMaxY;
+
+    prevX = mouseX;
+    prevY = mouseY;
   }
-  prevX = mouseX;
-  prevY = mouseY;
+
 }
 
 function sleep(ms) {
@@ -56,69 +86,104 @@ function sleep(ms) {
 }
 
 async function mouseReleased() {
-  prevX = undefined;
-  prevY = undefined;
+  if (mouseInCanvas()) {
+    prevX = undefined;
+    prevY = undefined;
 
-  predictionText.html('i\'m still guessing !');
-  await sleep(100);
-  predict();
-  console.log('mouse released!');
-}
-
-function predict() {
-  if (knnClassifier !== undefined) { // still loading
+    predictionText.html('i\'m still guessing !');
+    await sleep(100);
     // transform the canvas to input
     let input = transformCanvasToFeatures();
+    predict(input);
+    // console.log('mouse released!');
+  }
+}
 
+function predict(input) {
+  if (knnClassifier !== undefined) { // still loading
     // use the model to predict the digit
-    knnClassifier.classify(input, gotResults);
+    knnClassifier.classify(input, 5, gotResults); // k = 5
   }
 }
 
 function gotResults(err, result) {
-  // Display any error
-  // if (err) console.error(err);
-
-  if (result.confidencesByLabel) {
-    const confidences = result.confidencesByLabel;
-    console.log(confidences);
+  if (result.confidences) {
+    const confidences = result.confidences;
+    const confidence = (confidences[result.label] * 100).toFixed(2);
 
     if (result.label) {
-      predictionText.html('i think that this is a ' + result.label);
+      predictionText.html('i think that this is a ' + result.label + ' with a confidence of ' + confidence + ' %');
     }
   }
 }
+
+// ~~ converts a float to an integer
+// for more visit https://stackoverflow.com/questions/34077449/fastest-way-to-cast-a-float-to-an-int-in-javascript/34077505
 
 function transformCanvasToFeatures() {
   loadPixels();
   let d = pixelDensity();
 
-  let f = [];
-  for (let x = 0; x < nbSquare; x++) {
-    for (let y = 0; y < nbSquare; y++) {
-      const left = x * nbPixelsInSquare;
-      const top = y * nbPixelsInSquare;
+  const w = maxTouchPos.x - minTouchPos.x;
+  const h = maxTouchPos.y - minTouchPos.y;
+  const nbPixelsW = ~~(w / nbSquare);
+  const nbPixelsH = ~~(h / nbSquare);
 
-      const grayValue = getRectAvg(left, top, nbPixelsInSquare, d);
-      // ~~ converts a float to an integer
-      // for more visit https://stackoverflow.com/questions/34077449/fastest-way-to-cast-a-float-to-an-int-in-javascript/34077505
+  let sample = [];
+  for (let row = 0; row < nbSquare; row++) {
+    for (let col = 0; col < nbSquare; col++) {
+      const left = minTouchPos.x + col * nbPixelsW;
+      const top = minTouchPos.y + row * nbPixelsH;
 
-      f.push(~~map(grayValue, 0, 255, 0, 16));
+      const grayValue = getRectAvg(left, top, nbPixelsW, nbPixelsH, d);
+      sample.push(~~map(grayValue, 0, 255, 0, 16));
     }
   }
-  console.log(f);
-  // console.log(f.length);
-  return f;
+  // console.log(sample);
+  return sample;
 }
 
-function getRectAvg(left, top, pixels, density) {
+var img;
+function drawModelView() {
+  noStroke();
+  drawSample(transformCanvasToFeatures(), 0, 0);
+  setDrawingProperties();
+}
+
+function drawSample(sample, x, y, img) {
+  for (let row = 0; row < nbSquare; row++) {
+    for (let col = 0; col < nbSquare; col++) {
+      drawSampleValue(sample, x, y, row, col, img);
+    }
+  }
+}
+function drawSampleValue(values, sampleX, sampleY, row, col, img) {
+  const smallEdgeSize = 10;
+  const x = sampleX + col * smallEdgeSize;
+  const y = sampleY + row * smallEdgeSize;
+
+  var index = values[col + row * nbSquare];
+
+  const grayValue = map(index, 0, 16, 0, 255);
+
+  if (img === undefined) {
+    fill(grayValue);
+    rect(x, y, smallEdgeSize, smallEdgeSize);
+  } else {
+    img.noStroke();
+    img.fill(grayValue);
+    img.rect(x, y, smallEdgeSize, smallEdgeSize);
+  }
+}
+
+function getRectAvg(left, top, pixelsW, pixelsH, density) {
   var s = 0;
-  for (let i = 0; i < pixels; i++) {
-    for (let j = 0; j < pixels; j++) {
+  for (let i = 0; i < pixelsW; i++) {
+    for (let j = 0; j < pixelsH; j++) {
       s += getPixelGray(left + i, top + j, density);
     }
   }
-  return s / (pixels * pixels);
+  return s / (pixelsW * pixelsH);
 }
 
 function logCanvasPixels() {
@@ -131,8 +196,8 @@ function logCanvasPixels() {
       p.push(getPixelGray(i, j, d));
     }
   }
-  console.log(p);
-  console.log(p.length);
+  // console.log(p);
+  // console.log(p.length);
 }
 
 function getPixelGray(x, y, density) {
